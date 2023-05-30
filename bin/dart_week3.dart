5dbc9f7d84125351ed156ae30ead9cd99c3b1a04
yankorn <oranweang@gmail.com>
2023-05-30 13:12:10 +0700

    first app

---
+++ bin/dart_week3.dart
@@ -0,0 +1,5 @@
+import 'package:dart_week3/dart_week3.dart' as dart_week3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_week3.calculate()}!');
+}
---
+++ test/dart_week3_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_week3/dart_week3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
